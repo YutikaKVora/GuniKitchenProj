d36d88e8b5be1d14fe3956e2970643c0a0dfeaaa
Yutika Vora <vorayutika@outlook.com>
2022-03-03 19:04:18 +0530

    Added Products Model and Enum Product Category and migrations as well.

---
+++ GuniKitchen/Data/ProductCategory.cs
@@ -0,0 +1,8 @@
+﻿namespace GuniKitchen.Data
+{
+    public enum ProductCategory
+    {
+        Pizza = 1,
+        Burger
+    }
+}
---
+++ GuniKitchen/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using GuniKitchen.Data;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GuniKitchen.Models
+{
+        [Table("Products")]
+        public class Product
+        {
+            [Key]
+            [Display(Name = "Product ID")]
+            public int ProductId { get; set; }
+
+
+            [Display(Name = "Product Name")]
+            public string ProductName { get; set; }
+
+            public string Description { get; set; }
+
+            public double Price { get; set; }
+
+            public string ImageURL { get; set; }
+
+            public ProductCategory ProductCategory { get; set; }
+        }
+}
